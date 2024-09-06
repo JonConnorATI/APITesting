cfdff792b012efffd147d0d0267256ed90a3f85e
jonathan.connor <jonathan.connor@propylon.com>
2024-09-06 16:53:44 +0100

    updates

--- Resources/consts-user01.py
+++ Resources/consts-user01.py
@@ -1,2 +1,11 @@
-# offBeat base url
-baseURL = 'https://offbeatdonuts.com/order-now'
+"""
+Variables for the API testing
+"""
+
+baseURL = 'https://restful-booker.herokuapp.com/'
+authURL = baseURL + 'auth'
+bookingURL = baseURL + 'booking/'
+
+# Log in details for the API
+username = 'admin'
+password = 'password123'
